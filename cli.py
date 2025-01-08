baa178bab2605c2ea08cc9518ed0ea6862976d66
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-01-08 23:13:06 +0800

    update langchain and chat model service

--- cli.py
+++ cli.py
@@ -1,8 +1,68 @@
 #!/usr/bin/env python3
-import fire
+import os
 import time
 
-from tldr.tldr import tldr_from_url
+
+import fire
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_community.chat_models import ChatZhipuAI
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_community.document_transformers import Html2TextTransformer
+from langchain_openai.chat_models import ChatOpenAI
+
+load_dotenv()
+
+
+def init_chat(service="default"):
+    if service == "default":
+        service = os.environ.get("SERVICE", "zhipuai")
+
+    if service == "zhipuai":
+        model = os.environ.get("MODEL", "glm-4-flash")
+
+        chat = ChatZhipuAI(
+            model="glm-4-flash",
+            temperature=0.01
+        )
+    elif service == "deepseek":
+        model = os.environ.get("MODEL", "deepseek-chat")
+        chat = ChatOpenAI(
+            model="deepseek-chat",
+            temperature=0.01,
+            base_url="https://api.deepseek.com")
+    else:
+        chat = None
+        model = "None"
+    print(f"模型服务: {service}")
+    print(f"模型类型: {model}")
+    return chat
+
+
+def tldr_from_url(url: str) -> str:
+
+    loader = AsyncHtmlLoader(url)
+    html = loader.load()
+    html2text = Html2TextTransformer()
+    page = html2text.transform_documents(html)
+
+    prompt = f"{page} 介绍了 {url}, 请用中文介绍什么是 {url}。"
+
+    messages = [
+        SystemMessage(
+            content="你是一个信息总结专家，你可以根据网页内容总结关键信息。"
+        ),
+        HumanMessage(
+            content=prompt
+        )
+    ]
+
+    chat = init_chat()
+
+    response = chat.invoke(messages)
+
+    return response.content
+
 
 def cli(url: str) -> None:
 
